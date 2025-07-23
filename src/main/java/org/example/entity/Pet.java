2af021a944a15bc38325d12cd371a91b0ec6c25e
fanzi03 <tirolrol15@gmail.com>
2025-07-23 11:56:13 +0800

    clean a bit

--- src/main/java/org/example/entity/Pet.java
+++ src/main/java/org/example/entity/Pet.java
@@ -1,17 +1,25 @@
 package org.example.entity;
 
-import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.Year;
+import org.example.enums.Gender;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-
-import java.time.LocalDate;
-import java.time.Year;
-
-import org.example.enums.Gender;
 
 @Data
 @NoArgsConstructor
@@ -21,6 +29,7 @@ import org.example.enums.Gender;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Pet {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
